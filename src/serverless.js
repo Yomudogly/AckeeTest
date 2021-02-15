5b8c4420737d867b14b477058332474e7db14db7
Yomudogly <khalnepesov@gmail.com>
2021-02-14 23:17:13 -0500

    cors fix

--- src/serverless.js
+++ src/serverless.js
@@ -1,36 +1,36 @@
-'use strict'
+'use strict';
 
-const { ApolloServer } = require('apollo-server-lambda')
+const { ApolloServer } = require('apollo-server-lambda');
 
-const config = require('./utils/config')
-const connect = require('./utils/connect')
-const createApolloServer = require('./utils/createApolloServer')
-const { createServerlessContext } = require('./utils/createContext')
+const config = require('./utils/config');
+const connect = require('./utils/connect');
+const createApolloServer = require('./utils/createApolloServer');
+const { createServerlessContext } = require('./utils/createContext');
 
 if (config.dbUrl == null) {
-	throw new Error('MongoDB connection URI missing in environment')
+  throw new Error('MongoDB connection URI missing in environment');
 }
 
-connect(config.dbUrl)
+connect(config.dbUrl);
 
 const apolloServer = createApolloServer(ApolloServer, {
-	context: createServerlessContext
-})
+  context: createServerlessContext,
+});
 
 const origin = (() => {
-	if (config.allowOrigin === '*') {
-		return true
-	}
+  if (config.allowOrigin === '*') {
+    return true;
+  }
 
-	if (config.allowOrigin != null) {
-		return config.allowOrigin.split(',')
-	}
-})()
+  if (config.allowOrigin != null) {
+    return config.allowOrigin.split(',');
+  }
+})();
 
 exports.handler = apolloServer.createHandler({
-	cors: {
-		origin,
-		methods: 'GET,POST,PATCH,OPTIONS',
-		allowedHeaders: 'Content-Type, Authorization, Time-Zone'
-	}
-})
+  cors: {
+    origin: true,
+    methods: 'GET,POST,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, Time-Zone',
+  },
+});
